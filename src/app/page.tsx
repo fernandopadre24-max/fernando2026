0d355c7465d89bc2db215a71e6888259eadc2016
fernando sena <fernandopadre24@gmail.com>
2025-08-12 01:20:40 +0000

    expandir mais um pouco tela de Criar Novo Evento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -198,12 +198,12 @@ export default function Home() {
   return (
     <AppShell>
       <main className="container mx-auto px-4 pb-16">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start mb-8">
-          <div className="lg:col-span-2 flex flex-col gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-5 gap-8 items-start mb-8">
+          <div className="lg:col-span-3 flex flex-col gap-8">
              <DashboardSummary events={events} />
              <RecentTransfers events={events} bankAccounts={bankAccounts} />
           </div>
-          <div className="lg:col-span-1">
+          <div className="lg:col-span-2">
             <EventForm
               artists={artists}
               contractors={contractors}
